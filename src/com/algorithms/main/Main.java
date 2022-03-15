420f746e2da1553e8c5f286c2e5e8957dbdf492d
kchauhan <kanika.chauhan@ymail.com>
2022-03-15 14:28:37 +0530

    Adding files to initial commit

---
+++ src/com/algorithms/main/Main.java
@@ -0,0 +1,5 @@
+package com.algorithms.main;
+
+public class Main {
+
+}
---
+++ test/com/algorithms/main/Test.java
@@ -0,0 +1,12 @@
+package com.algorithms.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
