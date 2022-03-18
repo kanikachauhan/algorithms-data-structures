42999c950d1db932d277ddcc196a4b233c67cfa1
kchauhan <kanika.chauhan@ymail.com>
2022-03-18 16:27:40 +0530

    adding some implementations of list

--- src/com/algorithms/list/SingleLinkedList.java
+++ src/com/algorithms/list/SingleLinkedList.java
@@ -1,7 +1,12 @@
 package com.algorithms.list;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Stack;
+
 /**
  * Node data structure to store data for a particular node in a list
+ * 
  * @author kchauhan
  *
  * @param <T>
@@ -10,8 +15,8 @@ class Node<T> {
 
 	Node<T> next;
 	T data;
-	
-	public Node(T data){
+
+	public Node(T data) {
 		this.data = data;
 		this.next = null;
 	}
@@ -19,6 +24,7 @@ class Node<T> {
 
 /**
  * Single linked list implementation
+ * 
  * @author kchauhan
  *
  * @param <T>
@@ -26,191 +32,369 @@ class Node<T> {
 public class SingleLinkedList<T extends Comparable<T>> {
 
 	Node<T> head;
-	
+
 	/**
 	 * adding to the end of list
+	 * 
 	 * @param data
 	 */
 	public void append(T data) {
-		if(head == null) {
+		if (head == null) {
 			head = new Node<T>(data);
 			return;
 		}
 		Node<T> temp = head;
-		while(temp.next!=null) {
+		while (temp.next != null) {
 			temp = temp.next;
 		}
-		temp.next = new Node<T>(data);	
+		temp.next = new Node<T>(data);
 	}
 
 	/**
 	 * searching an element
+	 * 
 	 * @param data
 	 * @return
 	 */
-	public  boolean search(T data) {
-		if(head.data.compareTo(data)==0)
+	public boolean search(T data) {
+		if (head.data.compareTo(data) == 0)
 			return true;
-		Node<T> temp =  (Node<T>) head;
-		while(temp!=null) {
-			if(temp.data.compareTo(data)==0) {
+		Node<T> temp = (Node<T>) head;
+		while (temp != null) {
+			if (temp.data.compareTo(data) == 0) {
 				return true;
 			}
 			temp = temp.next;
 		}
 		return false;
 	}
-	
+
 	/**
 	 * searching using recursion
+	 * 
 	 * @param temp
 	 * @param data
 	 * @return
 	 */
-	public boolean searchRecur(Node<T> temp,T data) {
-		if(temp==null)
+	public boolean searchRecur(Node<T> temp, T data) {
+		if (temp == null)
 			return false;
-		if(temp.data.compareTo(data)==0)
+		if (temp.data.compareTo(data) == 0)
 			return true;
 		return searchRecur(temp.next, data);
 	}
-	
+
 	/**
 	 * insert before a node in a list
+	 * 
 	 * @param data
 	 * @param beforeNode
 	 */
-	public void insertBefore(T data,Node<T> beforeNode) {
+	public void insertBefore(T data, Node<T> beforeNode) {
 		Node<T> newNode = new Node<T>(data);
-		if(beforeNode==head) {
+		if (beforeNode == head) {
 			newNode.next = head;
 			head = newNode;
 			return;
 		}
 		Node<T> temp = head;
-		while(temp.next!=beforeNode) {
+		while (temp.next != beforeNode) {
 			temp = temp.next;
 		}
 		newNode.next = temp.next;
 		temp.next = newNode;
 	}
-	
+
 	/**
 	 * insert after a node in a list
+	 * 
 	 * @param data
 	 * @param afterNode
 	 */
-	public void insertAfter(T data,Node<T> afterNode) {
+	public void insertAfter(T data, Node<T> afterNode) {
 		Node<T> newNode = new Node<T>(data);
-		if(afterNode==head) {
+		if (afterNode == head) {
 			newNode.next = head;
 			head = newNode;
 			return;
 		}
 		Node<T> temp = head;
-		while(temp!=afterNode) {
+		while (temp != afterNode) {
 			temp = temp.next;
 		}
 		newNode.next = afterNode.next;
 		afterNode.next = newNode;
 	}
-	
+
 	/**
 	 * index of an element in a list
+	 * 
 	 * @param data
 	 * @return
 	 */
 	public int indexOf(T data) {
 		int ind = -1;
 		Node<T> temp = head;
-		while(temp!=null) {
-			ind = ind+1;
-			if(temp.data.compareTo(data)==0)
+		while (temp != null) {
+			ind = ind + 1;
+			if (temp.data.compareTo(data) == 0)
 				return ind;
 			temp = temp.next;
 		}
-		if(temp==null)
+		if (temp == null)
 			return -1;
 		return ind;
 	}
-	
+
 	/**
 	 * size of a list
+	 * 
 	 * @return
 	 */
 	public int size() {
-		if(head==null)
+		if (head == null)
 			return 0;
 		int count = 0;
 		Node<T> temp = head;
-		while(temp!=null) {
+		while (temp != null) {
 			count++;
 			temp = temp.next;
 		}
 		return count;
 	}
-	
+
 	/**
 	 * recursive implementation to calculate size of a list
+	 * 
 	 * @param node
 	 * @param count
 	 * @return
 	 */
-	public int sizeRecur(Node<T> node,int count) {
-		if(node==null)
+	public int sizeRecur(Node<T> node, int count) {
+		if (node == null)
 			return count;
-		count = count+1;
+		count = count + 1;
 		return sizeRecur(node.next, count);
 	}
-	
+
 	/**
 	 * get nthnode of a list
+	 * 
 	 * @param index
 	 * @return
 	 */
-	public Node<T> get(int index){
-		if(index == -1)
+	public Node<T> get(int index) {
+		if (index == -1)
 			return null;
-		int count  = 0;
+		int count = 0;
 		Node<T> temp = head;
-		while(count!=index) {
-			count = count+1;
+		while (count != index) {
+			count = count + 1;
 			temp = temp.next;
 		}
 		return temp;
 	}
-	
+
 	/**
 	 * nth node from the end of the list
+	 * 
 	 * @param n
 	 * @return
 	 */
-	public Node<T> nthNodeEnd(int n){
+	public Node<T> nthNodeEnd(int n) {
 		Node<T> first = head;
 		Node<T> second = head;
 		int count = 0;
-		while(count!=n) {
-			count = count+1;
+		while (count != n) {
+			count = count + 1;
 			first = first.next;
 		}
-		while(first!=null) {
+		while (first != null) {
 			second = second.next;
 			first = first.next;
 		}
 		return second;
 	}
-	
+
+	public Node<T> findMiddle() {
+		Node<T> first = head;
+		Node<T> second = head;
+		while (first != null && second.next != null && second != null) {
+			first = first.next;
+			second = second.next.next;
+		}
+		return first;
+	}
+
 	/**
 	 * printing all elements of a list
 	 */
 	public void print() {
 		Node<T> temp = head;
-		while(temp!=null) {
-			System.out.println(temp.data);
+		while (temp != null) {
+			System.out.print(temp.data + " ");
 			temp = temp.next;
+		}
+		System.out.println();
+	}
+
+	/**
+	 * find occurrences of a int a list
+	 */
+	public int findOccurrences(int data) {
+		Node<T> temp = head;
+		int count = 0;
+		while (temp != null) {
+			if ((Integer) temp.data == data) {
+				count++;
+			}
+			temp = temp.next;
+		}
+		return count;
+	}
+
+	/**
+	 * detect loop in a list
+	 */
+	public boolean detectLoop() {
+		boolean flag = false;
+		Node<T> first = head;
+		Node<T> second = head;
+		while (first != null && second.next != null && second != null) {
+			first = first.next;
+			second = second.next.next;
+			if (first == second) {
+				flag = true;
+				break;
+			}
+		}
+		return flag;
+
+	}
+
+	/**
+	 * delete list
+	 */
+	public void deleteList() {
+		this.head = null;
+	}
+
+	/**
+	 * delete node
+	 */
+	public void deleteNode(Node<T> node) {
+		if (node == head) {
+			head = head.next;
+			return;
+		}
+		Node<T> temp = head;
+		while (temp.next != node) {
+			temp = temp.next;
+		}
+		if (temp.next.next == null) {
+			temp.next = null;
+			return;
+		}
+		temp.next = temp.next.next;
+	}
+
+	/**
+	 * delete at a given position
+	 */
+	public void deleteAtPosition(int t) {
+		Node<T> temp = head;
+		if (t == 0) {
+			head = temp.next;
+			return;
+		}
+		Node<T> prev = null;
+		for (int i = 0; i < t && temp != null; i++) {
+			prev = temp;
+			temp = temp.next;
+		}
+		prev.next = temp.next;
+	}
+
+	/**
+	 * length of a loop in a list
+	 */
+	public int lengthLoop() {
+		int count = 0;
+		Node<T> first = head;
+		Node<T> second = head;
+		while (first != null && second.next != null && second != null) {
+			first = first.next;
+			second = second.next.next;
+			count = count+1;
+			if (first == second) {
+				break;
+			}
+		}
+		return count;
+	}
+	
+	/**
+	 * check if list is palindrome or not
+	 */
+	public boolean isPalindrome() {
+		Node<Integer> temp = (Node<Integer>) head;
+		Stack<Integer> stack = new Stack<>();
+		while(temp!=null) {
+			stack.push(temp.data);
+			temp = temp.next;
+		}
+		boolean flag = true;
+		temp = (Node<Integer>) head;
+		while(!stack.isEmpty()) {
+			if(stack.pop()!=temp.data) {
+				flag = false;
+				break;
+			}
+			temp = temp.next;
+		}
+		return flag;
+	}
+
+	/**
+	 * remove duplicates
+	 */
+	public void removeDuplicates() {
+		Node<T> temp = head;
+		while(temp!=null) {
+			Node<T> curr = temp;
+			while(curr!=null && curr.data==temp.data) {
+				curr = curr.next;
+			}
+			temp.next = curr;
+			temp  = temp.next;
 		}
 	}
 	
+	/**
+	 * remove from unsorted list
+	 */
+	public void removeDuplicatesUnsorted() {
+		//TODO
+	}
 	
+	/**
+	 * move last element to the front of list
+	 */
+	public void moveLastToFront() {
+		Node<T> temp = head;
+		while(temp.next.next!=null) {
+			temp = temp.next;
+		}
+		Node newHead = temp.next;
+		newHead.next = head;
+		head = newHead;
+		temp.next = null;
+	}
 	
+	/**
+	 * intersection of two sorted lists
+	 */
+	public void intersectionTwoSortedLists() {
+		
+	}
 }
--- test/com/algorithms/list/SingleLinkedListTest.java
+++ test/com/algorithms/list/SingleLinkedListTest.java
@@ -3,6 +3,7 @@ package com.algorithms.list;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -12,15 +13,18 @@ import org.junit.jupiter.api.Test;
 import com.algorithms.list.SingleLinkedList;
 
 public class SingleLinkedListTest {
-	
-	SingleLinkedList<Integer> list;
-	
+
+	private SingleLinkedList<Integer> list,palindromeList,duplicateList;
+
 	@BeforeEach
 	public void setUp() {
 		list = new SingleLinkedList<Integer>();
-		list.append(1); list.append(2); list.append(3); list.append(4);
+		palindromeList = new SingleLinkedList<Integer>();
+		duplicateList = new SingleLinkedList<Integer>();
+		list.append(1);list.append(2);list.append(3);list.append(4);list.append(5);
+		palindromeList.append(1);palindromeList.append(3); palindromeList.append(3); palindromeList.append(1);
+		duplicateList.append(1);duplicateList.append(1);duplicateList.append(2);duplicateList.append(2);duplicateList.append(3);duplicateList.append(4);duplicateList.append(5);duplicateList.append(5);
 	}
-	
 
 	/**
 	 * tests append,search,indexof,get methods
@@ -39,21 +43,21 @@ public class SingleLinkedListTest {
 		list.insertAfter(2000, list.head.next.next);
 		assertTrue(list.search(2000));
 		assertEquals("Index difference", 3, list.indexOf(2000));
-		assertEquals("Index difference", -1,list.indexOf(102324));
-		assertEquals("Difference in value",2000 ,(int)(list.get(3).data));
-		assertEquals("Difference in value",1000 ,(int)(list.get(0).data));
-		assertEquals("Difference in value",200, (int)(list.nthNodeEnd(3).data));
+		assertEquals("Index difference", -1, list.indexOf(102324));
+		assertEquals("Difference in value", 2000, (int) (list.get(3).data));
+		assertEquals("Difference in value", 1000, (int) (list.get(0).data));
+		assertEquals("Difference in value", 3, (int) (list.nthNodeEnd(3).data));
 	}
-	
+
 	/**
 	 * tests length calculations
 	 */
 	@Test
 	public void testLength() {
-		assertEquals(4, list.size());
-		assertEquals(4, list.sizeRecur(list.head, 0));
+		assertEquals(5, list.size());
+		assertEquals(5, list.sizeRecur(list.head, 0));
 	}
-	
+
 	/**
 	 * test searching
 	 */
@@ -63,4 +67,97 @@ public class SingleLinkedListTest {
 		assertFalse(list.searchRecur(list.head, 13));
 		assertTrue(list.searchRecur(list.head, 3));
 	}
+
+	/**
+	 * test middle of list
+	 */
+	@Test
+	public void testMiddle() {
+		assertEquals("Difference in expected value", Integer.valueOf(3), list.findMiddle().data);
+	}
+
+	/**
+	 * test occurrences
+	 */
+	@Test
+	public void testOccurrences() {
+		list.append(2);
+		list.append(2);
+		list.append(2);
+		assertEquals("Difference in expected value", 4, list.findOccurrences(2));
+	}
+
+	/**
+	 * test loop
+	 */
+	@Test
+	public void testLoop() {
+		Node temp = list.head;
+		while (temp.next != null) {
+			temp = temp.next;
+		}
+		temp.next = list.head;
+		assertTrue("Difference in expected value", list.detectLoop());
+		assertEquals("Difference in expected value", 5, list.lengthLoop());
+	}
+
+	/**
+	 * test list deletion
+	 */
+	@Test
+	public void testListDeletion() {
+		list.deleteList();
+		assertNull("Difference in expected value", list.head);
+	}
+	
+	/**
+	 * test node deletion
+	 */
+	@Test
+	public void testNodeDeletion() {
+		list.deleteNode(list.head);
+		assertTrue("Difference in expected value", (list.indexOf(1)==-1));
+		list.deleteNode(list.head.next);
+		assertTrue("Difference in expected value",(list.indexOf(3)==-1));
+		list.deleteNode(list.head.next.next);
+		assertTrue("Difference in expected value",(list.indexOf(5)==-1));
+	}
+	
+	/**
+	 * test node deletion at a position
+	 */
+	@Test
+	public void testNodeDeletionAtPosition() {
+		list.append(6); list.append(7); list.append(8); list.append(9); list.append(10); list.append(11);
+		list.deleteAtPosition(5);
+		assertTrue("Difference in expected value",(list.size()==10));
+	}
+	
+	/**
+	 * test list is palindrome or not
+	 */
+	@Test
+	public void testPalindrome() {
+		assertTrue("Difference in expected value", palindromeList.isPalindrome());
+		assertFalse("Difference in expected value", list.isPalindrome());
+	}
+	
+	/**
+	 * test duplicates
+	 */
+	@Test
+	public void testDuplicates() {
+		duplicateList.removeDuplicates();
+		assertTrue("Difference in expected value",(duplicateList.size()==5));
+	}
+	
+	/**
+	 * test move last element to first
+	 */
+	@Test
+	public void testMoveLastToFirst() {
+		list.moveLastToFront();
+		assertEquals("Difference in expected value", 5, (int)list.head.data);
+	}
+	
 }
